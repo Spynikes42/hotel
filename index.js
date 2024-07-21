1d351026891f7b8ae23291e1b28e3f1b9e021111
tejas_sonawane <tejassonawane4242@gmail.com>
2024-07-21 21:22:09 +0530

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const path = require("path")
+const cors = require("cors")
+require("dotenv").config()
+
+mongoose.connect(process.env.MONGO_URL)
+const app = express()
+
+app.use(express.static("dist"))
+app.use(express.static("uploads"))
+app.use(express.json())
+app.use(cors({
+    origin: true,
+    credentials: true
+}))
+
+app.use("/api/auth", require("./routes/auth.routes"))
+app.use("/api/admin", require("./routes/Admin.route"))
+app.use("/api/order", require("./routes/order.route"))
+
+app.use("*", async (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "Resource Not found" })
+})
+
+app.use((error, req, res, next) => {
+    res.status(500).json({ message: error.message || "somthing went wrong" })
+})
+mongoose.connection.once("open", () => {
+    console.log("MONGO CONNECT")
+
+    app.listen(5000, console.log("SERVER RUNNING"))
+})
